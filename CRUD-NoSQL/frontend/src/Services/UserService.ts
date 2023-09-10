dc8113d2bcc466ce7161a6e986e1f78e0212bcf8
EstanisVictor <vitorestanislau1@gmail.com>
2023-09-10 10:46:41 -0300

    fix: mudando frontend para conectar ao 8080

--- CRUD-NoSQL/frontend/src/Services/UserService.ts
+++ CRUD-NoSQL/frontend/src/Services/UserService.ts
@@ -4,7 +4,7 @@ import { Api } from "providers";
 
 export function createUser(user: IUser) {
   // return Api.post<any>("/generate", user);
-  return fetch("http://localhost:5000/register", {
+  return fetch("http://localhost:8080/register", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -14,7 +14,7 @@ export function createUser(user: IUser) {
 }
 
 export function getAllUsers() {
-  return fetch("http://localhost:5000/getAll", {
+  return fetch("http://localhost:8080/getAll", {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
@@ -38,7 +38,7 @@ export function getAllUsers() {
 
 export async function getUserByID(userID : string | undefined) {
   console.log("id: "+userID);
-  return fetch(`http://localhost:5000/getUser/${userID?.trimEnd().trimStart()}`, {
+  return fetch(`http://localhost:8080/getUser/${userID?.trimEnd().trimStart()}`, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
@@ -52,7 +52,7 @@ export async function getUserByID(userID : string | undefined) {
   })
   .then(data => {
     console.log(data)
-    return data;
+    return data["data"];
   })
   .catch(error => {
     console.error(error);
@@ -64,7 +64,7 @@ export function deleteUser(userId: string | undefined) {
   if(userId === undefined){
     throw new Error('Erro ao deletar usuário');
   }
-  return fetch(`http://localhost:5000/delete/${userId}`, {
+  return fetch(`http://localhost:8080/delete/${userId}`, {
     method: "DELETE",
     headers: {
       "Content-Type": "application/json",
@@ -73,8 +73,9 @@ export function deleteUser(userId: string | undefined) {
 }
 
 export async function updateUser(userId : string | undefined | string[], updatedUser:IUser) {
-  const url = `http://localhost:5000/users/${userId}`; // Substitua "users" pelo endpoint correto da sua API
-
+  const url = `http://localhost:8080/users/${userId}`; // Substitua "users" pelo endpoint correto da sua API
+  console.log("url: "+updatedUser);
+  
   try {
     const response = await fetch(url, {
       method: "PUT",
@@ -83,11 +84,13 @@ export async function updateUser(userId : string | undefined | string[], updated
       },
       body: JSON.stringify(updatedUser),
     });
+
     if (!response.ok) {
-      throw new Error("Erro ao atualizar usuário");
+      const errorMessage = `Erro ao atualizar usuário: ${response.statusText}`;
+      throw new Error(errorMessage);
     }
-    const data = await response.json();
-    return data;
+    // const data = await response.json();
+    return response;
   } catch (error) {
     console.error(error);
     throw error;
--- CRUD-NoSQL/frontend/src/pages/tabela.tsx
+++ CRUD-NoSQL/frontend/src/pages/tabela.tsx
@@ -9,13 +9,20 @@ export default function Tabela() {
     router.push('/')
   };
 
+  const handleClickRefresh = () => {
+    window.location.reload();
+  };
+
   return(
     <>
     <Nav />
     <h1 className='text-center text-3xl mt-10 mb-7'>Informações</h1>
     <div className="flex items-center flex-col justify-center w-full">
       <EnhancedTable />
-      <ButtonPrimary onClick={handleClick}>Voltar</ButtonPrimary>
+      <div className="flex">
+        <span className="ml-2 mr-2"><ButtonPrimary onClick={handleClick}>Voltar</ButtonPrimary></span>
+        <span className="ml-2 mr-2"><ButtonPrimary onClick={handleClickRefresh}>Refresh</ButtonPrimary></span>
+      </div>
     </div>
     </>
   );
